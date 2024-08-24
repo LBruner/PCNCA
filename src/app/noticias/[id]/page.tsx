e21a162f50c9a8c4cfca4b534e5fe9af9921c861
Leandro Bruner <leandrobruner15@gmail.com>
2024-08-24 15:44:40 -0300

    adiciona página de demonstração de uma notícia

--- src/app/noticias/[id]/page.tsx
+++ src/app/noticias/[id]/page.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown';
-import { textoMD } from "@/app/noticias/[id]/Texto";
+import {db} from "@/db";
+import {notFound} from "next/navigation";
 
-const NoticiaShowPage: React.FC = () => {
+interface NoticiaPageProps{
+    params: {
+        id: string;
+    }
+}
+
+const NoticiaShowPage: React.FC<NoticiaPageProps> = async ({params}) => {
+    const noticia = await db.article.findFirst({
+        where: {
+            id: parseInt(params.id),
+        }
+    });
+
+    if(!noticia) return notFound();
+
     return (
         <div className="mt-28 prose w-full max-w-screen-lg mx-auto">
             <ReactMarkdown components={{
                 img: ({ node, ...props }) => (
                     <img {...props} className="mx-auto my-4" alt={props.alt} />
                 ),}} className="markdown-body">
-                {textoMD}
+                {noticia.content}
             </ReactMarkdown>
         </div>
 
