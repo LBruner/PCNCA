6a60e58bd7680872b7907756134244ae82c0f749
Leandro Bruner <leandrobruner15@gmail.com>
2024-09-08 20:46:19 -0300

    altera lógica de visualização de notícia para construção dinâmica do markdown

--- src/app/noticias/[id]/page.tsx
+++ src/app/noticias/[id]/page.tsx
@@ -1,34 +1,33 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown';
-import {db} from "@/db";
 import {notFound} from "next/navigation";
+import {getNoticia} from "@/actions/noticias";
+import {generateMarkdown} from "@/helpers/noticia/criacao/criar-noticia";
 
-interface NoticiaPageProps{
+interface NoticiaPageProps {
     params: {
         id: string;
     }
 }
 
 const NoticiaShowPage: React.FC<NoticiaPageProps> = async ({params}) => {
-    const noticia = await db.article.findFirst({
-        where: {
-            id: parseInt(params.id),
-        }
-    });
+    const noticia = await getNoticia(params.id);
 
-    if(!noticia) return notFound();
+    if (!noticia) return notFound();
+
+    const content = generateMarkdown(noticia.title, noticia.subtitle, noticia.imageUrl || '', noticia.content);
 
     return (
         <div className="mt-28 prose w-full max-w-screen-lg mx-auto">
             <ReactMarkdown components={{
-                img: ({ node, ...props }) => (
-                    <img {...props} className="mx-auto my-4" alt={props.alt} />
-                ),}} className="markdown-body">
-                {noticia.content}
+                img: ({node, ...props}) => (
+                    <img {...props} className="mx-auto my-4" alt={props.alt}/>
+                ),
+            }} className="markdown-body">
+                {content}
             </ReactMarkdown>
         </div>
-
     );
-};
+}
 
 export default NoticiaShowPage;
