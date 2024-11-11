be8538d1c2592b6d6a0de67da8595dcc0a1889f3
Leandro Bruner <leandrobruner15@gmail.com>
2024-11-11 17:10:41 -0300

    apagar componentes não utilizados da tela do carrosel de commodities

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,26 +1,10 @@
-import React, {Suspense} from "react";
-import {getCotacoes} from "@/actions/cotacoes";
-import Cotacoes from "@/components/cotacoes/cotacoes";
-import {Spinner} from "@nextui-org/react";
+import React from "react";
 
 const HomePage: React.FC = () => {
     return (
         <div className={'h-screen flex flex-col'}>
-            <Suspense fallback={<Spinner/>}>
-                <CotacoesWrapper />
-            </Suspense>
+            <h1>oifsa</h1>
         </div>
     )
 }
-
-const CotacoesWrapper: React.FC = async () => {
-    const cotacoes = await getCotacoes();
-
-    if(cotacoes?.length == 0 || !cotacoes){
-        return <p>Nenhum produto encontrado</p>
-    }
-
-    return <Cotacoes cotacoes={cotacoes} />;
-}
-
 export default HomePage;
