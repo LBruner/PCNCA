ef5877dd9ce4c0391f7723372554f8d8098f8c53
Leandro Bruner <leandrobruner15@gmail.com>
2024-11-17 15:46:01 -0300

    Torna aba de vendas acessível apenas a usuários logados

--- src/components/UI/navbar/custom-navbar-body.tsx
+++ src/components/UI/navbar/custom-navbar-body.tsx
@@ -37,7 +37,7 @@ const CustomNavbarBody: React.FC<CustomNavbarBodyProps> = ({user}) => {
                 </div>
                 <NavbarContent className="hidden sm:flex gap-4" justify="center">
                     <NavLink path={paths.noticias()} text={'Notícias'} />
-                    <NavLink path={paths.vendas()} text={'Vendas'} />
+                    {user && <NavLink path={paths.vendas()} text={'Vendas'} />}
                     <NavLink path={paths.cotacoesCommodities()} text={'Cotações'} />
                     {user && <NavLink path={paths.estoque()} text={'Estoque'} />}
                     {user && <NavLink path={paths.pessoas()} text={'Pessoas'} />}
