2f77fa4a39ca3af5dbdaad8e88972dd21d43964d
Leandro Bruner <leandrobruner15@gmail.com>
2025-02-27 09:44:54 -0300

    limpa migrations antigas

--- prisma/migrations/20240824165046_change_field_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `profilePictureUrl` on the `Author` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Author" DROP COLUMN "profilePictureUrl",
-ADD COLUMN     "profile_picture_url" TEXT;
