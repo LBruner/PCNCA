3be3e48a16137924f162d948ebec43f18055f564
Leandro Bruner <leandrobruner15@gmail.com>
2025-03-03 12:19:02 -0300

    adiciona fallback na tela de vendas

--- src/app/vendas/page.tsx
+++ src/app/vendas/page.tsx
@@ -3,11 +3,15 @@ import {pegaTodasVendas} from "@/actions/vendas";
 import {pegaTodosClientes} from "@/actions/clientes";
 import {pegaTodosProdutosQueVenderam} from "@/actions/produto";
 import VendasPageBody from "@/components/vendas/VendasPageBody";
+import NoData from "@/components/UI/NoData";
 
 const VendasPage: React.FC = async _ => {
     const vendas = await pegaTodasVendas();
     const clientes = await pegaTodosClientes();
     const produtos = await pegaTodosProdutosQueVenderam();
+
+    if(vendas.length == 0){
+        return  <NoData description={'Nenhuma venda encontrada'}/>   }
 
     if (clientes[0] != null && clientes[1] != null) {
         clientes[0].pessoas.push(...clientes[1].pessoas)
